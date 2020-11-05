8c6ca8d0bca5aead8dff756bb3abe4dc3fdc6ed0
Aditya Verma <aaadarths2357@gmail.com>
2020-11-05 20:12:37 +0530

    Added: code to calculate the monthly wage of the employee till a condition of total working hours of 160 or maximum days of 20 is reached

--- employeeWage.js
+++ employeeWage.js
@@ -4,6 +4,7 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const TOTAL_MONTHLY_WORKING_DAYS = 20;
+const TOTAL_MONTHLY_WORKING_HOURS = 160;
 
 function getWorkingHours(employeeCheck) {
     switch(employeeCheck){
@@ -17,11 +18,17 @@ function getWorkingHours(employeeCheck) {
 }
 
 let totalEmployeeHours = 0;
-for(let day = 0; day < TOTAL_MONTHLY_WORKING_DAYS; day++){
+let totalWorkingDays = 0;
+while(totalEmployeeHours <= TOTAL_MONTHLY_WORKING_HOURS && 
+      totalWorkingDays <= TOTAL_MONTHLY_WORKING_DAYS) {
     let employeeCheck = Math.floor(Math.random() * 10) % 3;
     totalEmployeeHours += getWorkingHours(employeeCheck);
+    totalWorkingDays++;
 }
 
 let totalEmployeeWage = totalEmployeeHours * WAGE_PER_HOUR;
+console.log("Total Days : " + (totalWorkingDays-1) + "\t" 
+          + "Total Working Hours : " 
+          + (totalEmployeeHours > 160 ? 160 : totalEmployeeHours));
 console.log("Monthly Employee Wage : Rs " + totalEmployeeWage);
 
