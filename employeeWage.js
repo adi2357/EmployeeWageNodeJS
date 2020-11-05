daa664a4592257013dd047422f378fed2d558f3c
Aditya Verma <aaadarths2357@gmail.com>
2020-11-05 19:56:47 +0530

    Added: code to calculate total monthly employee wage assuming 20 working days in a month

--- employeeWage.js
+++ employeeWage.js
@@ -3,24 +3,25 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const TOTAL_MONTHLY_WORKING_DAYS = 20;
 
 function getWorkingHours(employeeCheck) {
     switch(employeeCheck){
         case IS_PART_TIME :
-            console.log("Employee is PRESENT.");
             return PART_TIME_HOURS;
         case IS_FULL_TIME :
-            console.log("Employee is PRESENT.");
             return FULL_TIME_HOURS;
         default :
-            console.log("Employee is ABSENT.");
             return 0;
     }
 }
 
-let employeeHours = 0;
-let employeeCheck = Math.floor(Math.random() * 10) % 3;
-employeeHours = getWorkingHours(employeeCheck);
-let employeeWage = employeeHours * WAGE_PER_HOUR;
-console.log("Employee Wage : Rs " + employeeWage);
+let totalEmployeeHours = 0;
+for(let day = 0; day < TOTAL_MONTHLY_WORKING_DAYS; day++){
+    let employeeCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmployeeHours += getWorkingHours(employeeCheck);
+}
+
+let totalEmployeeWage = totalEmployeeHours * WAGE_PER_HOUR;
+console.log("Monthly Employee Wage : Rs " + totalEmployeeWage);
 
