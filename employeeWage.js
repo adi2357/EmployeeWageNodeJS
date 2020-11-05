20b3eac76d0327d4d8f2b8f520bec23f283e2892
Aditya Verma <aaadarths2357@gmail.com>
2020-11-05 19:45:43 +0530

    Refactored: code to add a function to compute the daily working of the employee and calculte the employee wage based on the working hours

--- employeeWage.js
+++ employeeWage.js
@@ -1,33 +1,26 @@
-{
-    const  IS_ABSENT = 0;
-    let employeeCheck = Math.floor(Math.random() * 10) % 2;
-    if (employeeCheck == 0) {
-        console.log("Employee is ABSENT. Exiting the Program...");
-        return;
-    } else {
-        console.log("Employee is PRESENT");
-    }
-}
-
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let employeeHours = 0;
-let employeeCheck = Math.floor(Math.random() * 10) % 3;
-switch(employeeCheck){
-    case IS_PART_TIME :
-         employeeHours = PART_TIME_HOURS;
-         break;
-    case IS_FULL_TIME :
-        employeeHours = FULL_TIME_HOURS;
-        break;
-    default :
-        employeeHours = 0;
+function getWorkingHours(employeeCheck) {
+    switch(employeeCheck){
+        case IS_PART_TIME :
+            console.log("Employee is PRESENT.");
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME :
+            console.log("Employee is PRESENT.");
+            return FULL_TIME_HOURS;
+        default :
+            console.log("Employee is ABSENT.");
+            return 0;
+    }
 }
 
+let employeeHours = 0;
+let employeeCheck = Math.floor(Math.random() * 10) % 3;
+employeeHours = getWorkingHours(employeeCheck);
 let employeeWage = employeeHours * WAGE_PER_HOUR;
 console.log("Employee Wage : Rs " + employeeWage);
 
