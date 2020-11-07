5b8f53a939c0b52fc994fa94e5b6eda61de0916d
Aditya Verma <aaadarths2357@gmail.com>
2020-11-07 17:31:51 +0530

    Added: code to display days along with daily wages when full time wage of 160 is earned using array filter function

--- employeeWage.js
+++ employeeWage.js
@@ -35,6 +35,7 @@ while(totalEmployeeHours <= TOTAL_MONTHLY_WORKING_HOURS &&
     totalWorkingDays++;
 }
 
+
 let dayCounter = 0;
 function dayToDailyWageMap(dailyWage) {
     dayCounter++;
@@ -44,9 +45,11 @@ let dayToDailyWageMapArray = employeeDailyWageArray.map(dayToDailyWageMap);
 console.log("Daily Wage Map :");
 console.log(dayToDailyWageMapArray);
 
+
 console.log("Total Days : " + totalWorkingDays + "\t" 
           + "Total Working Hours : " 
           + (totalEmployeeHours > 160 ? 160 : totalEmployeeHours));
+
 
 let totalEmployeeWage = 0;
 function sum(dailyWage){
@@ -55,7 +58,17 @@ function sum(dailyWage){
 employeeDailyWageArray.forEach(sum);
 console.log("Monthly Employee Wage using ForEach : Rs " + totalEmployeeWage);
 
+
 function totalWages(totalWage, dailyWage){
     return totalWage + dailyWage;
 }
 console.log("Monthly Employee Wage using Reduce : Rs " + employeeDailyWageArray.reduce(totalWages, 0));
+
+
+function fullTimeWages(dailyWage){
+    return dailyWage.includes("160");
+}
+let fullTimeWageArray = dayToDailyWageMapArray.filter(fullTimeWages);
+console.log("Daily Wages when employees worked Full Time : ");
+console.log(fullTimeWageArray);
+
