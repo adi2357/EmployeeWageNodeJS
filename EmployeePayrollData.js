c0dddfc80db930472c70fe80114b4ae0a03ba4e9
Aditya Verma <aaadarths2357@gmail.com>
2020-11-10 15:19:22 +0530

    Added: code to extend employee payroll data to store gender and start date and used spread operator to initialise the fields through constructor

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,12 +1,11 @@
 class EmployeePayrollData{
 
-    id;
-    salary;
-
-    constructor(id,name, salary){
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+    constructor(...parameters){
+        this.id = parameters[0];
+        this.name = parameters[1];
+        this.salary = parameters[2];
+        this.gender = parameters[3];
+        this.startDate = parameters[4];
     }
 
     get name(){
@@ -18,8 +17,11 @@ class EmployeePayrollData{
     }
 
     toString() {
-        return "id: " + this.id + ", name: " + this.name + ", salary: " + this.salary + "\n";
-    }
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const employeeDate = !this.startDate ? "undefined" :
+                             this.startDate.toLocaleDateString("en-US", options);
+        return "id: " + this.id + ", name: " + this.name + ", salary: " + this.salary 
+            + ", gender: " + this.gender + ", startDate: " + employeeDate + "\n";}
 }
 
 let employeePayrollData = new EmployeePayrollData(1, "Mark", 50000);
@@ -28,5 +30,8 @@ console.log(employeePayrollData.toString());
 employeePayrollData.id = 0;
 employeePayrollData.name = "Jeff";
 console.log(employeePayrollData.toString());
+
+let newEmployeePayrollData = new EmployeePayrollData(1, "Terrisa", 80000, "F", new Date());
+console.log(newEmployeePayrollData.toString());
 
 
